e857136b93ee4a4255417d47fbe623a6fd11e144
Vivek <vivekpareek096@gmail.com>
2018-12-29 14:54:45 +0530

    Made LocationPersistencetest to use LocationJpaRepository

--- src/main/java/com/guitar/db/repository/LocationJpaRepository.java
+++ src/main/java/com/guitar/db/repository/LocationJpaRepository.java
@@ -1,5 +1,7 @@
 package com.guitar.db.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import com.guitar.db.model.Location;
 
 @Repository
 public interface LocationJpaRepository extends JpaRepository<Location, Long> {
-
+	List<Location> findByStateLike(String stateName);
 }
